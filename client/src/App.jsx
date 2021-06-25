6044711e75f3de56a5baba2ee55bb069cee35507
Filopateer Maged <filopateermaghed@gmail.com>
2021-06-25 12:42:40 +0200

    react routes and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import {BrowserRouter as Router,Switch, Route} from "react-router-dom"
+import Home from './Routes/Home'
+import RestauratnDetailPage from './Routes/RestauratnDetailPage'
+import Update from './Routes/Update'
 
 const App = () => {
-    return <div>App</div>
+    return <div>
+      
+        <Router>
+        <Route exact path  ="/" component={Home}/>
+        <Route exact path  ="/restaurants/:id/update" component={Update}/>
+        <Route exact path  ="/restaurants/:id" component={RestauratnDetailPage}/>
+        </Router>
+     
+        </div>
 }
 export default App;
---
+++ client/src/Components/RestaurantList.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const RestaurantList = () => {
+    return (
+        <div className="">
+         <table class="table table-hover table-dark">
+  <thead>
+    <tr className="table-success">
+      <th scope="col">Restaurant</th>
+      <th scope="col">Location</th>
+      <th scope="col">Price Range</th>
+      <th scope="col">Ratings</th>
+      <th scope="col">Edit</th>
+      <th scope="col">Delete</th>
+    </tr>   
+    </thead>
+    <tbody>
+        <tr>
+        <th  scope="row">Zaza</th>
+        <td>Masr el gedida</td>
+        <td>$</td>
+        <td>star</td>
+        <td><button className="btn btn-warning">Edit</button></td>
+        <td><button className="btn btn-danger">Delete</button></td>
+        </tr>
+    </tbody>
+    </table>
+        </div>
+    )
+}
+
+export default RestaurantList
