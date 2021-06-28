02e34b5736906fdaad4e9d0bf76b4be48a351e39
Filopateer Maged <filopateermaghed@gmail.com>
2021-06-28 12:32:41 +0200

    navbar v1

--- client/src/Routes/Home.jsx
+++ client/src/Routes/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Header from '../Components/Header'
 import AddRestaurant from '../Components/AddRestaurant'
 import RestaurantList from '../Components/RestaurantList'
+import Navbar from '../Components/Navbar'
 const Home = () => {
     return (
         <div>
+            <Navbar/>
            <Header/>
            <AddRestaurant/>
            <RestaurantList/>
